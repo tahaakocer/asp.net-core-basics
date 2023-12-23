a73fd310248f68500fb961526d610651262626a6
tahaakocer <taha.kocer317@gmail.com>
2023-12-23 04:14:36 +0300

    feedback and app list

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -7,18 +7,28 @@ namespace Basics.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var model = Repository.Applications;
+            return View(model);
         }
 
         public IActionResult Apply()
         {
             return View();
         }
-        
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Apply([FromForm] Candidate model)
         {
+            if(Repository.Applications.Any(c => c.Email.Equals(model.Email))){
+                ModelState.AddModelError("","There is already an application for you.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                Repository.Add(model);
+                return View("Feedback", model);
+            }
             return View();
         }
     }
--- Models/Candidate.cs
+++ Models/Candidate.cs
@@ -1,14 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Basics.Models
 {
     public class Candidate
     {
-        public String Email { get; set; } =String.Empty;
-        public String FirstName { get; set; } =String.Empty;
-        public String LastName { get; set; } =String.Empty;
+        [Required(ErrorMessage ="Email is required.")]
+        public String? Email { get; set; } =String.Empty;
 
-        public String FullName => $"{FirstName} {LastName.ToUpper()}";
+        [Required(ErrorMessage ="First name is required.")]
+        public String? FirstName { get; set; } =String.Empty;
+
+        [Required(ErrorMessage ="Last name is required.")]
+        public String? LastName { get; set; } =String.Empty;
+
+        public String? FullName => $"{FirstName} {LastName?.ToUpper()}";
+
+        [Required(ErrorMessage ="Age is required.")]
         public int? Age { get; set; }
-        public String SelectedCourse { get; set; } =String.Empty;
+        public String? SelectedCourse { get; set; } =String.Empty;
         public DateTime ApplyAt { get; set; }
         public Candidate(){
             ApplyAt = DateTime.Now;
